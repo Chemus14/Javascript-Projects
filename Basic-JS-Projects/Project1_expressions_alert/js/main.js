5c5dbb037f83f27a8b5e78fe2c2c1adf3889a674
Chemus14 <75949407+Chemus14@users.noreply.github.com>
2023-04-21 12:47:49 +0100

    Basic JS Projects Assignment

---
+++ Basic-JS-Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// pop up an alert with Hello World
+// window.alert('Hello world');
+
+// Display text in the html Hello World
+
+//document.write('Hello World');
+
+// Variables
+let A = 'This is a string';
+document.write(A);
+// alert(A);
+
+// Scape Character
+document.write(
+  'Lisa told Bart, "Knock it off, Bart! or I\'ll tell dad!" <br> "Eat my shorts!" Bart responded.'
+);
+
+// Concatenate
+
+const B = 'hi';
+const C = 'How are you?';
+const D = 'Cool';
+
+// What is displaying is an expression
+document.write(B + ' ' + C + ' Good! See you tomorrow ' + D);
+
+const E = 'Javascript' + D;
+document.write(E);
+
+// Another way to declare variables
+const family = 'Jimenez',
+  dad = 'Jose',
+  mom = 'Marisa',
+  brother = 'Luis';
+
+document.write(dad);
+
+// Basic JS Projects Assignment
+
+const Sent1 = 'This is the beginning of the string';
+alert(Sent1);
+const Sent2 = ' and this is the end of the string';
+alert(Sent2);
+
+document.write('<br>' + Sent1 + Sent2);
